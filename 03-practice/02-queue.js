7d0d7c47f5a575382a43110be6d6adc2cbf0895f
Golo Roden <golo.roden@thenativeweb.io>
2023-05-22 16:42:19 +0200

    Add queue.

---
+++ 03-practice/02-queue.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const createQueue = function () {
+    const data = [];
+
+    const isEmpty = function () {
+        // ...
+    };
+
+    const enqueue = function (x) {
+        // ...
+    };
+
+    const dequeue = function () {
+        // ...
+    };
+
+    const peek = function () {
+        // ...
+    };
+
+    return { isEmpty, enqueue, dequeue, peek };
+};
